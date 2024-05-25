04cb4adcbbd3e459e33ebfd345fe93d1f6f08cdc
leemungai <mrmungai@yahoo.com>
2024-05-25 20:24:06 +0200

    putting-all-together lab

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,20 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand, size):
+      self.brand = brand
+      self.size = size
+
+    def cobble(self):
+        print("Your shoe has been repaired.")
+        self.condition = "New"  
+    @property
+    def size(self):
+       return self._size 
+    
+    @size.setter
+    def size(self,size):
+        if isinstance(size, int):
+          self._size = size
+        else:
+          print("size must be an integer")
